29e73e5e2cdd1b3be78b041c0ffe85103d9d2fa1
Priyanshu Singh <priyanshusinghajm@gmail.com>
2024-01-07 20:10:48 +0530

    weather_app_2

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4673ff5616msh2f8a0ffaed6cf8fp19496ajsne0c05e720653',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'api.openweathermap.org/data/2.5/'
+
+export const WEATHER_API_KEY = '7438e333ee0bc655ee3029ded3618dc7'
