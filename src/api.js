9c09449b73127e429dee7102120e1138d9e6288a
Priyanshu Singh <priyanshusinghajm@gmail.com>
2024-01-07 20:24:48 +0530

    gitignore

--- src/api.js
+++ src/api.js
@@ -11,3 +11,4 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 export const WEATHER_API_URL = 'api.openweathermap.org/data/2.5/'
 
 export const WEATHER_API_KEY = '7438e333ee0bc655ee3029ded3618dc7'
+ 
